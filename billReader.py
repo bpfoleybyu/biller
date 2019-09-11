3700424de4c1058b1c8a2529cdfa69ba07acdbf1
Branden Foley <brandenfoley@gmail.com>
2019-09-10 22:07:13 -0600

    changed to PIL.ImageGrab, proj on hold for a while

--- billReader.py
+++ billReader.py
@@ -6,40 +6,45 @@
 
 import tkinter as tk
 import tkinter.messagebox
+import pyscreenshot as ImageGrab
+import datetime
+import PIL.ImageGrab
 from PIL import ImageTk, Image
 from urllib.request import urlopen
 
 # pull in image, either from url or local
 #error check it, display it.
-def doThing():
-    link=tk.Entry.get(E1)
-    tk.messagebox.showinfo("got link: ",link)
-    try:
-        if "http" in link:
-            im = Image.open(urlopen(link))
-        else:
-            im=Image.open(link)
-        im.show()
-        panel = tk.Label(window, image = im)
-    except IOError:
-        tk.messagebox.showerror("ERROR", "bad image/image link")
+# def doThing():
+#     link=tk.Entry.get(E1)
+#     tk.messagebox.showinfo("got link: ",link)
+#     try:
+#         if "http" in link:
+#             im = Image.open(urlopen(link))
+#         else:
+#             im=Image.open(link)
+#         im.show()
+#         panel = tk.Label(window, image = im)
+#     except IOError:
+#         tk.messagebox.showerror("ERROR", "bad image/image link")
+#     return
+
+def catchScreen():
+    #use pil.imagegrab instead
+    im = PIL.ImageGrab.grab()
+    im.save('test.png')
+    
+    return
     
 
 window = tk.Tk()
 window.title("BillReader")
 window.geometry("300x300")
-path="default.png"
-img = ImageTk.PhotoImage(Image.open(path))
+# img = ImageTk.PhotoImage(Image.open(path))
 # cv = tk.Canvas()
 # cv.create_image(10,10,image=img, anchor=CENTER)
 # cv.grid(row=2, column=0)
 
 L1 = tk.Label(window, text="Bill Reader",).grid(row=0, column=1)
-L2 = tk.Label(window, text="Image url/link",).grid(row=1, column=0)
-panel = tk.Label(window, image = img)
-E1 = tk.Entry(window, bd=5)
-E1.grid(row=1, column=1)
-E2 = tk.Entry(window, bd=5)
-B = tk.Button(window, text="Submit", command=doThing).grid(row=2, column=1,)
+B = tk.Button(window, text="Grab Screen", command=catchScreen).grid(row=2, column=1)
 
 window.mainloop()
